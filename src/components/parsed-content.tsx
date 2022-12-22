79031dfe33fa8e75a698154d4309135fcf98a849
David Granado <davidjgranado@gmail.com>
2022-12-22 00:35:02 -0600

    Debug to fix vercel timeouts

--- src/components/parsed-content.tsx
+++ src/components/parsed-content.tsx
@@ -1,67 +1,70 @@
-
-import { Link as MuiLink } from '@mui/material';
-import { Interweave } from 'interweave';
-import { polyfill } from 'interweave-ssr';
-import Link from 'next/link';
-import { forwardRef } from 'react';
-import {
-	UrlMatcher,
-	HashtagMatcher,
-	HashtagProps,
-	UrlProps,
-} from 'interweave-autolink';
-import { Paths } from '@common/constants';
-
-polyfill();
-
-interface Props {
-	children: string;
-}
-
 export
-function ParsedContent(props: Props) {
-	const { children } = props;
+// Debug to see if this is causing vercel timeouts
+const ParsedContent = (props: any) => <>{props.children}</>;
 
-	return (
-		<Interweave
-			content={children}
-			matchers={[new UrlMatcher('url', { validateTLD: false }, Url), new HashtagMatcher('hashtag', {}, Hashtag)]}
-		/>
-	);
-}
+// import { Link as MuiLink } from '@mui/material';
+// import { Interweave } from 'interweave';
+// import { polyfill } from 'interweave-ssr';
+// import Link from 'next/link';
+// import { forwardRef } from 'react';
+// import {
+// 	UrlMatcher,
+// 	HashtagMatcher,
+// 	HashtagProps,
+// 	UrlProps,
+// } from 'interweave-autolink';
+// import { Paths } from '@common/constants';
 
-function Url(props: UrlProps) {
-	const { url } = props;
+// polyfill();
 
-	return (
-		<MuiLink href={url} target="__blank">
-			{url}
-		</MuiLink>
-	);
-}
+// interface Props {
+// 	children: string;
+// }
 
-function Hashtag(props: HashtagProps) {
-	const { hashtag } = props;
+// export
+// function ParsedContent(props: Props) {
+// 	const { children } = props;
 
-	return (
+// 	return (
+// 		<Interweave
+// 			content={children}
+// 			matchers={[new UrlMatcher('url', { validateTLD: false }, Url), new HashtagMatcher('hashtag', {}, Hashtag)]}
+// 		/>
+// 	);
+// }
 
-		<MuiLink
-			href={`${Paths.Search}?q=${encodeURIComponent(hashtag)}`}
-			component={
-				// Hackhackhack
-				forwardRef<HTMLAnchorElement>(
-					(p, ref) => (
-						<Link
-							ref={ref}
-							href=""
-							{...p}
-						/>
-					)
-				)
-			}
-		>
-			{hashtag}
-		</MuiLink>
+// function Url(props: UrlProps) {
+// 	const { url } = props;
 
-	);
-}
+// 	return (
+// 		<MuiLink href={url} target="__blank">
+// 			{url}
+// 		</MuiLink>
+// 	);
+// }
+
+// function Hashtag(props: HashtagProps) {
+// 	const { hashtag } = props;
+
+// 	return (
+
+// 		<MuiLink
+// 			href={`${Paths.Search}?q=${encodeURIComponent(hashtag)}`}
+// 			component={
+// 				// Hackhackhack
+// 				forwardRef<HTMLAnchorElement>(
+// 					(p, ref) => (
+// 						<Link
+// 							ref={ref}
+// 							href=""
+// 							{...p}
+// 						/>
+// 					)
+// 				)
+// 			}
+// 		>
+// 			{hashtag}
+// 		</MuiLink>
+
+// 	);
+// }
