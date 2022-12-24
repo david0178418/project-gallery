59b9d5fa2ce58ae219b2f16ae71cacb710b4b0e2
David Granado <davidjgranado@gmail.com>
2022-12-23 22:16:13 -0600

    Reorg

--- src/components/layout/left-rail/index.tsx
+++ src/components/layout/left-rail/index.tsx
@@ -1,16 +1,14 @@
 import Link from 'next/link';
 import { useRouter } from 'next/router';
 import { useSession } from 'next-auth/react';
-import {
-	MouseEvent, ReactNode, useState,
-} from 'react';
+import { ReactNode } from 'react';
 import { urlJoin } from '@common/utils';
+import { CreateDropdown } from './create-dropdown';
 import {
 	ModalActions,
 	Paths,
 } from '@common/constants';
 import {
-	AddIcon,
 	HomeActiveIcon,
 	LoginIcon,
 	HomeIcon,
@@ -18,18 +16,13 @@ import {
 	ProfileIcon,
 	SettingsActiveIcon,
 	SettingsIcon,
-	ProjectIcon,
-	JournalIcon,
 } from '@components/icons';
 import {
-	Fab,
 	List,
 	ListItem,
 	ListItemButton,
 	ListItemIcon,
 	ListItemText,
-	Menu,
-	MenuItem,
 } from '@mui/material';
 
 interface Props {
@@ -64,7 +57,7 @@ function RailButtonContent(props: Props) {
 }
 
 // TODO Implement better path matching for active icon
-
+export
 function LeftRail() {
 	const router = useRouter();
 	const { data } = useSession();
@@ -168,100 +161,6 @@ function LeftRail() {
 					</>
 				)}
 			</List>
-		</>
-	);
-}
-
-export { LeftRail };
-
-function CreateDropdown() {
-	const router = useRouter();
-	const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
-	const open = Boolean(anchorEl);
-
-	const {
-		pathname,
-		query,
-	} = router;
-
-	function handleEl(el: MouseEvent<HTMLElement>) {
-		setAnchorEl(el.currentTarget);
-	}
-
-	function handleClose() {
-		setAnchorEl(null);
-	}
-
-	return (
-		<>
-			<Fab
-				color="primary"
-				sx={{
-					display: {
-						xs: 'inline-flex',
-						md: 'none',
-					},
-				}}
-				onClick={handleEl}
-			>
-				<AddIcon/>
-			</Fab>
-			<Fab
-				color="primary"
-				variant="extended"
-				sx={{
-					width: '100%',
-					display: {
-						xs: 'none',
-						md: 'inline-flex',
-					},
-				}}
-				onClick={handleEl}
-			>
-				<AddIcon sx={{ mr: 1 }} />
-				Create
-			</Fab>
-			<Menu
-				anchorEl={anchorEl}
-				open={open}
-				onClose={handleClose}
-				MenuListProps={{ 'aria-labelledby': 'basic-button' }}
-			>
-				<Link
-					shallow
-					href={{
-						pathname,
-						query: {
-							a: ModalActions.CreateProject,
-							...query,
-						},
-					}}
-				>
-					<MenuItem onClick={handleClose}>
-						<ListItemIcon>
-							<ProjectIcon fontSize="small" />
-						</ListItemIcon>
-						Project
-					</MenuItem>
-				</Link>
-				<Link
-					shallow
-					href={{
-						pathname,
-						query: {
-							a: ModalActions.CreateJournal,
-							...query,
-						},
-					}}
-				>
-					<MenuItem onClick={handleClose}>
-						<ListItemIcon>
-							<JournalIcon fontSize="small" />
-						</ListItemIcon>
-						Journal Post
-					</MenuItem>
-				</Link>
-			</Menu>
 		</>
 	);
 }
