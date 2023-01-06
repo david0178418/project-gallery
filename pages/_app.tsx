aa5c809456e18c6070b671eaf6b8c1e495427a0c
David Granado <davidjgranado@gmail.com>
2023-01-05 21:29:48 -0600

    Add social metatags

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -13,6 +13,7 @@ import { Session } from 'next-auth';
 import dynamic from 'next/dynamic';
 import { Layout } from '@components/layout';
 import { Analytics } from '@vercel/analytics/react';
+import { DefaultSeo } from 'next-seo';
 
 interface Props {
 	session: Session | null;
@@ -43,13 +44,25 @@ function App(props: AppProps<Props>) {
 
 	return (
 		<>
+			<DefaultSeo
+				openGraph={{
+					type: 'ProjectGallery.me',
+					locale: 'en_IE',
+					url: 'https://projectgallery.me/',
+					siteName: 'ProjectGallery.me',
+					title: 'ProjectGallery.me',
+					description: 'Your work, your stories - Share it with the world on ProjectGallery.me!',
+				}}
+				twitter={{
+					handle: '@justdavidg',
+					cardType: 'summary',
+				}}
+			/>
 			<Head>
 				<link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png"/>
 				<link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png"/>
 				<link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png"/>
 				<link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5"/>
-				<meta name="msapplication-TileColor" content="#da532c"/>
-				<meta name="theme-color" content="#ffffff"/>
 				<meta
 					name="viewport"
 					content="initial-scale=1, width=device-width"
