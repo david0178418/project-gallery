2682ddaf9e0a4da47588bff7a91c1c2413dd6b8a
David Granado <davidjgranado@gmail.com>
2023-01-22 10:54:47 -0600

    Fix loading bug

--- src/components/loader.tsx
+++ src/components/loader.tsx
@@ -13,6 +13,9 @@ function Loader() {
 		<Backdrop
 			open={loading}
 			sx={{
+				// Hax fix for issue with backdrop not
+				// being removed wihen open is false
+				display: loading ? 'flex' : 'none',
 				color: '#fff',
 				zIndex: (theme) => theme.zIndex.modal + 1,
 			}}
