3b742148a9d81db0cad55d7a6150deb390d6fc5d
David Granado <davidjgranado@gmail.com>
2023-02-12 17:56:56 -0600

    Just pass props through

--- src/components/label.tsx
+++ src/components/label.tsx
@@ -5,21 +5,13 @@ type Props = ComponentProps<typeof Chip>;
 
 export default
 function Label(props: Props) {
-	const {
-		label,
-		onClick,
-	} = props;
-
 	return (
 		<Box
 			marginBottom={1}
 			marginRight={1}
 			display="inline-block"
 		>
-			<Chip
-				onClick={onClick}
-				label={label}
-			/>
+			<Chip {...props} />
 		</Box>
 	);
 }
