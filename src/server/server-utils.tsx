22794ed7faabb646d6373a3a3d2d50dd032ca8cc
David Granado <davidjgranado@gmail.com>
2023-02-18 20:47:56 -0600

    Add email service to docker and utility fn

--- src/server/server-utils.tsx
+++ src/server/server-utils.tsx
@@ -1,5 +1,37 @@
 
+import { createTransport } from 'nodemailer';
+import { Address } from 'nodemailer/lib/mailer';
+
+const {
+	SMTP_PORT = '25',
+	SMTP_PW = '',
+	SMTP_SERVER = '',
+	SMTP_USERNAME = '',
+} = process.env;
+
+const auth = (SMTP_USERNAME) ?
+	{
+		user: SMTP_USERNAME,
+		pass: SMTP_PW,
+	} :
+	undefined;
+
+interface SendEmailArgs {
+	from: string | Address;
+	html: string;
+	subject: string;
+	text: string;
+	to: string | string[] | Address | Address[];
+}
+
 export
-function foo() {
-	return null;
+async function sendEmail(args: SendEmailArgs) {
+	const transporter = createTransport({
+		host: SMTP_SERVER,
+		port: +SMTP_PORT,
+		auth,
+	});
+
+	// send mail with defined transport object
+	return await transporter.sendMail(args);
 }
