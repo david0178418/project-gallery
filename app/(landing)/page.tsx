902feb06509ce47fdafd926236ccaf76da676805
David Granado <davidjgranado@gmail.com>
2023-02-27 23:19:39 -0600

    WIP

--- app/(landing)/page.tsx
+++ app/(landing)/page.tsx
@@ -1,14 +1,26 @@
 'use client';
 
-import { LogoInvertedImage } from '@common/images';
+import { LogoSmall } from '@common/images';
+import Image from 'next/image';
 import {
 	AppBar,
 	Box,
+	Button,
 	Container,
+	Drawer,
+	Grid,
+	IconButton,
+	List,
+	ListItem,
+	ListItemButton,
+	ListItemIcon,
+	ListItemText,
 	Toolbar,
 	Typography,
 } from '@mui/material';
-import Image from 'next/image';
+import Link from 'next/link';
+import { useState } from 'react';
+import { LoginIcon, MenuIcon } from '@components/icons';
 
 export default
 function HomePage() {
@@ -16,10 +28,33 @@ function HomePage() {
 		<>
 			<TopMenu />
 			<Container>
-				<Box textAlign="center">
-					<Typography>
-						Your Project, Your Story.
+				<Box textAlign="center" marginTop={15}>
+					<Typography
+						variant="h1"
+						fontWeight="bold"
+						fontSize={50}
+					>
+						<Box color="primary.main">
+							Your Project<br/>
+						</Box>
+						Your Story
 					</Typography>
+					<Grid
+						container
+						direction="row"
+						justifyContent="center"
+					>
+						<Grid item xs={5}>
+							<Box component="ul" textAlign="left" fontSize={16}>
+								<Box component="li">
+									Createyour own personal project portfolio
+								</Box>
+								<Box component="li">
+									Keep a journal
+								</Box>
+							</Box>
+						</Grid>
+					</Grid>
 				</Box>
 			</Container>
 		</>
@@ -27,30 +62,122 @@ function HomePage() {
 }
 function TopMenu() {
 	return (
-		<AppBar position="static" elevation={0}>
-			<Container maxWidth="xl">
+		<AppBar
+			position="fixed"
+			elevation={0}
+			sx={{
+				backdropFilter: 'blur(8px)',
+				backgroundColor: 'rgba(255,255,255,0.8)',
+				boxShadow: 'inset 0px -1px 1px #ddd',
+			}}
+		>
+			<Container>
 				<Toolbar disableGutters>
-
-					<Box
-						component="a"
-						sx={{ width: '100%' }}
-					>
-						<Image
-							alt=""
-							src={LogoInvertedImage}
-						/>
+					<Box width="100%" display="flex">
+						<Link href="/" >
+							<Box display="flex" gap={1}>
+								<Image
+									alt=""
+									src={LogoSmall}
+								/>
+								<Typography
+									fontSize={20}
+									fontWeight="bold"
+									sx={{
+										display: {
+											xs: 'none',
+											sm: 'inline',
+										},
+									}}
+								>
+									ProjectGallery.me
+								</Typography>
+							</Box>
+						</Link>
 					</Box>
-					<Box sx={{
-						flexGrow: 1,
-						display: {
-							xs: 'none',
-							md: 'flex',
-						},
-					}}>
-						{' '}
+					<Box
+						sx={{
+							display: {
+								xs: 'none',
+								sm: 'flex',
+							},
+						}}
+					>
+						<Link href="https://projectgallery.me/project/63a490bfacb4e70acc9931bb/journals">
+							<Button sx={{ marginRight: 1 }}>
+								Blog
+							</Button>
+						</Link>
+						<Link href="/">
+							<Button
+								variant="outlined"
+								sx={{ marginRight: 1 }}
+							>
+								Login
+							</Button>
+						</Link>
+						<Link href="/">
+							<Button
+								variant="contained"
+							>
+								Register
+							</Button>
+						</Link>
+					</Box>
+					<Box
+						sx={{
+							display: {
+								xs: 'block',
+								sm: 'none',
+							},
+						}}
+					>
+						<Menu/>
 					</Box>
 				</Toolbar>
 			</Container>
 		</AppBar>
+	);
+}
+
+function Menu() {
+	const [open, setOpen] = useState(false);
+
+	return (
+		<>
+			<IconButton onClick={() => setOpen(true)}>
+				<MenuIcon/>
+			</IconButton>
+			<Drawer
+				anchor="left"
+				open={open}
+				onClose={() => setOpen(false)}
+			>
+				<Box width={250}>
+					<List>
+						<ListItem disablePadding>
+							<ListItemButton>
+								<ListItemIcon>
+									<LoginIcon/>
+								</ListItemIcon>
+								<ListItemText>
+									Login
+								</ListItemText>
+							</ListItemButton>
+						</ListItem>
+						<ListItem disablePadding>
+							<ListItemButton>
+								<ListItemIcon>
+									<LoginIcon/>
+								</ListItemIcon>
+								<ListItemText>
+									Register
+								</ListItemText>
+							</ListItemButton>
+						</ListItem>
+					</List>
+				</Box>
+			</Drawer>
+		</>
 	);
 }
--- src/common/images/index.ts
+++ src/common/images/index.ts
@@ -1,4 +1,4 @@
 export { default as LogoImage } from './logo.png';
 export { default as LogoInvertedImage } from './logo-inverted.png';
-export { default as LogoSmall } from './logo.png';
-export { default as LogoSmallInvertedImage } from './logo.png';
+export { default as LogoSmall } from './logo-small.png';
+export { default as LogoSmallInvertedImage } from './logo-small-inverted.png';
