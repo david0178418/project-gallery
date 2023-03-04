398166286fd350d5eaf30a9b08b2d2426e59808b
David Granado <davidjgranado@gmail.com>
2023-03-03 18:17:25 -0600

    fix metadata

--- app/(landing)/page.tsx
+++ app/(landing)/page.tsx
@@ -1,6 +1,28 @@
 import TopMenuSection from './top-menu.section';
 import Hero from './page.hero';
 import Features from './page.features';
+import { AppName, BaseUrl } from '@common/constants';
+import { LogoMain } from '@common/images';
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+	title: AppName,
+	twitter: {
+		creator: '@justdavidg',
+		card: 'summary_large_image',
+		images: [{ url: LogoMain.src }],
+	},
+	openGraph: {
+		type: 'website',
+		locale: 'en_IE',
+		url: BaseUrl,
+		siteName: AppName,
+		title: AppName,
+		description: 'Your work, your story - Share it with the world on ProjectGallery.me!',
+		images: [{ url: LogoMain.src }],
+	},
+	description: 'Your work, your story - Share it with the world on ProjectGallery.me!',
+};
 
 export default
 function LandingPage() {
--- src/common/images/index.ts
+++ src/common/images/index.ts
@@ -2,3 +2,4 @@ export { default as LogoImage } from './logo.png';
 export { default as LogoInvertedImage } from './logo-inverted.png';
 export { default as LogoSmall } from './logo-small.png';
 export { default as LogoSmallInvertedImage } from './logo-small-inverted.png';
+export { default as LogoMain } from './logo-main.png';
