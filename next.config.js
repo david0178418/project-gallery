bb7f94d3e54aac98103c8244a35996a0908a4925
David Granado <davidjgranado@gmail.com>
2023-09-28 20:43:42 -0500

    Commit next stuff

--- next.config.js
+++ next.config.js
@@ -2,18 +2,6 @@
 const nextConfig = {
 	reactStrictMode: true,
 	swcMinify: true,
-	async rewrites() {
-		return {
-			beforeFiles: [],
-			afterFiles: [],
-			fallback: [
-				{
-					source: '/:path*',
-					destination: '/content/:path*',
-				},
-			],
-		};
-	},
 	experimental: { serverActions: true },
 };
 
