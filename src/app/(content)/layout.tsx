374c8afb05f469b6dd9d57e74b1abc1edeb671a1
David Granado <davidjgranado@gmail.com>
2023-09-30 09:58:09 -0500

    Adjust layout

--- src/app/(content)/layout.tsx
+++ src/app/(content)/layout.tsx
@@ -1,19 +1,68 @@
-import { PageFrame } from '@components/page-frame';
-import { getServerSession } from '@server/auth-options';
 import { ReactNode } from 'react';
+import { LeftRail } from './page-frame/left-rail';
+import { BottomNav } from './page-frame/bottom-nav';
+import { Container, Grid } from '@ui';
 
 interface Props {
-	children: ReactNode;
+	children?: ReactNode;
 }
 
 export default
-async function ContentLayout(props: Props) {
+function ContentLayout(props: Props) {
 	const { children } = props;
-	const session = await getServerSession();
 
 	return (
-		<PageFrame session={session}>
-			{children}
-		</PageFrame>
+		<Container
+			sx={{
+				display: 'flex',
+				height: '100vh',
+				overflow: 'hidden',
+				paddingX: {
+					xs: 0,
+					sm: 1,
+					lg: 2,
+				},
+			}}
+		>
+			<Grid
+				container
+				columns={16}
+				spacing={2}
+				marginY={0}
+			>
+				<Grid
+					item
+					sm={2}
+					md={4}
+					sx={{
+						display: {
+							xs: 'none',
+							sm: 'block',
+						},
+					}}
+				>
+					<div>
+						{/** Wrapped in a random div due to this issue:
+							 * https://stackoverflow.com/questions/56347839/material-ui-v4-0-1-warning-expected-an-element-that-can-hold-a-ref
+							 */}
+						<LeftRail />
+					</div>
+				</Grid>
+				<Grid
+					item
+					xs={16}
+					sm={14}
+					md={12}
+					style={{
+						maxHeight: '100%',
+						overflow: 'hidden',
+						position: 'relative',
+					}}
+				>
+					{children}
+				</Grid>
+			</Grid>
+			<BottomNav/>
+		</Container>
 	);
 }
