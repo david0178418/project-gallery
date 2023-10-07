7c8cccf395699f949cd8ca229b33d393c2bd17e4
David Granado <davidjgranado@gmail.com>
2023-10-06 20:48:15 -0500

    Use named import

--- src/server/queries/index.ts
+++ src/server/queries/index.ts
@@ -7,7 +7,7 @@ import { DbCollections } from '@common/constants';
 import { DbJournal } from '@common/types/Journal';
 import { DbUserProfile } from '@common/types/UserProfile';
 import { DbUserGalleryOrder } from '@common/types/UserGalleryOrder';
-import add from 'date-fns/add';
+import { add } from 'date-fns';
 import { getServerSession } from '@server/auth-options';
 import {
 	makeId,
