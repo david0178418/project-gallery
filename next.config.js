79faa1464174da9ca4263491d24377e4090c6941
David Granado <davidjgranado@gmail.com>
2023-10-26 23:09:00 -0500

    Revert "Upgrade dependencies"
    This reverts commit e32650c7b2db3450db6966f02da8ec1c8c66d122.

--- next.config.js
+++ next.config.js
@@ -3,6 +3,7 @@ const nextConfig = {
 	reactStrictMode: true,
 	swcMinify: true,
 	experimental: {
+		serverActions: true,
 		optimizePackageImports: [
 			'@mui/icons-material',
 			'@mui/material',
