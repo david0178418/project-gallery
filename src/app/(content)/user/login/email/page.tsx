cd4e7f5aeffc2cf47eebf64db9c701fb1e919efb
David Granado <davidjgranado@gmail.com>
2023-12-28 17:21:56 -0600

    Adjust login forms

--- src/app/(content)/user/login/email/page.tsx
+++ src/app/(content)/user/login/email/page.tsx
@@ -1,19 +1,72 @@
-import LoginEmailForm from './login-email-form';
+'use client';
+import { useState } from 'react';
+import { Key } from 'ts-key-enum';
+import { useRouter } from 'next/navigation';
 import { Paths } from '@common/constants';
 import Link from 'next/link';
+import oneClickSendAction from './one-click-send-action';
+import TextField from '@components/common/text-field';
 import { Button } from '@components/ui/button';
+import { toast } from 'sonner';
+import { useSetLoading } from '@components/loader';
 
-export default function LoginPage() {
+export default
+function LoginEmailForm() {
+	const { replace } = useRouter();
+	const setLoading = useSetLoading();
+	const [email, setEmail] = useState('');
+	const valid = email.includes('@');
+
+	function handleKeyUp(key: string) {
+		if(key === Key.Enter) {
+			handleLogin();
+		}
+	}
+
+	async function handleLogin() {
+		setLoading(true);
+
+		try {
+			await oneClickSendAction(email);
+			toast(`A login link will be sent to "${email}" if an account with this email exists.`);
+			replace(Paths.Home);
+		} catch(e) {
+			toast('Something went wrong. Try again.');
+			console.log(e);
+		}
+
+		setLoading(false);
+	}
+
 	return (
 		<>
-			<div className="font-bold">
-				Login
-			</div>
-			<LoginEmailForm />
-			<div className="justify-center">
-				<Link replace href={Paths.UserRegister} >
-					<Button>
-						Create an Account
+			<form
+				noValidate
+				autoComplete="off"
+			>
+				<div>
+					<TextField
+						autoFocus
+						className="w-full"
+						type="text"
+						label="Email"
+						value={email}
+						onKeyUp={e => handleKeyUp(e.key)}
+						onChange={e => setEmail(e.target.value)}
+					/>
+				</div>
+			</form>
+			<div className="pt-3 grid gap-1 text-center">
+				<Button
+					className="w-full"
+					disabled={!valid}
+					onClick={handleLogin}
+				>
+					Send email link
+				</Button>
+				<Link href={Paths.UserLoginPw} replace>
+					<Button variant="link">
+						Sign in with Password
 					</Button>
 				</Link>
 			</div>
--- src/app/(content)/user/login/pw/page.tsx
+++ src/app/(content)/user/login/pw/page.tsx
@@ -1,22 +1,106 @@
-import LoginPwForm from './login-pw-form';
+'use client';
+import { useState } from 'react';
+import { Key } from 'ts-key-enum';
+import { login } from '@client/api-calls';
+import { inRange } from '@common/utils';
 import Link from 'next/link';
-import { Paths } from '@common/constants';
+import { useRouter } from 'next/navigation';
+import TextField from '@components/common/text-field';
 import { Button } from '@components/ui/button';
+import { toast } from 'sonner';
+import { useSetLoading } from '@components/loader';
+import {
+	Paths,
+	UsernameMaxLength,
+	UsernameMinLength,
+} from '@common/constants';
 
-export default function LoginModal() {
+export default
+function LoginPwForm() {
+	const setLoading = useSetLoading();
+	const { refresh } = useRouter();
+	const [usernameOrEmail, setUsernameOrEmail] = useState('');
+	const [password, setPassword] = useState('');
+	const validUsernameLength = inRange(usernameOrEmail.length, UsernameMinLength, UsernameMaxLength, true);
+	const valid = !!(
+		password &&
+		usernameOrEmail &&
+		validUsernameLength
+	);
+
+	function handleKeyUp(key: string) {
+		if(key === Key.Enter) {
+			handleLogin();
+		}
+	}
+
+	async function handleLogin() {
+		if(!valid) {
+			return;
+		}
+
+		setLoading(true);
+
+		try {
+			const results = await login(usernameOrEmail, password);
+
+			if(results?.ok) {
+				toast(`Logged in as ${usernameOrEmail}`);
+				refresh();
+			} else {
+				toast('Incorrect Login');
+			}
+
+		} catch(e) {
+			toast('Something went wrong. Try again.');
+			console.log(e);
+		}
+
+		setLoading(false);
+		setPassword('');
+	}
+
 	return (
 		<>
-			<div className="strong">
-				Login
-			</div>
-			<LoginPwForm />
-			<div className="flex justify-center">
-				<Link href={Paths.UserRegister} replace >
-					<Button>
-						Create an Account
+			<form
+				noValidate
+				autoComplete="off"
+			>
+				<div>
+					<TextField
+						autoFocus
+						className="w-full"
+						placeholder="username"
+						type="text"
+						label="Username or Email"
+						value={usernameOrEmail}
+						onKeyUp={e => handleKeyUp(e.key)}
+						onChange={e => setUsernameOrEmail(e.target.value)}
+					/>
+					<TextField
+						className="w-full"
+						label="Password"
+						type="password"
+						value={password}
+						onKeyUp={e => handleKeyUp(e.key)}
+						onChange={e => setPassword(e.target.value)}
+					/>
+				</div>
+				<div className="pt-3 grid gap-1 text-center">
+					<Button
+						className="w-full"
+						disabled={!valid}
+						onClick={handleLogin}
+					>
+						Sign in
 					</Button>
-				</Link>
-			</div>
+					<Link href={Paths.UserLoginEmail} replace>
+						<Button variant="link">
+							Sign in with Email Link
+						</Button>
+					</Link>
+				</div>
+			</form>
 		</>
 	);
 }
